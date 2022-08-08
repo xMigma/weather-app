fd55e92af84d48d4be50f0cc3da7d154c0250aa3
xMigma <miguelmh04@hotmail.com>
2022-08-08 14:17:46 +0200

    the default city is now assigned by ip

--- base/views.py
+++ base/views.py
@@ -2,21 +2,29 @@ import requests
 import json
 from urllib import response
 from django.shortcuts import render
+import json
 
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 API_KEY = '267d5a6861d7d54d0c3ed913fec66929'
 
 def index(request):
+    ip = requests.get('https://api.ipify.org').text
+    res = requests.get('http://ip-api.com/json/' + ip)
+
     if request.method == 'POST':
         city = request.POST.get('city')
-        url = BASE_URL + 'q=' + city + '&appid=' + API_KEY
-        response = requests.get(url).json()
-        context = {
-            'city': response['name'],
-            'country': response['sys']['country'],
-            'temperature': round(response['main']['temp'] - 273.15),
-            'description': response['weather'][0]['description'],
-            'icon': response['weather'][0]['icon']
-        }
-        return render(request, 'city.html', context)
-    return render(request, 'city.html')    
+    else:
+        city = res.json()['city']
+
+    url = BASE_URL + 'q=' + city + '&appid=' + API_KEY
+    response = requests.get(url).json()
+
+    context = {
+        'city': response['name'],
+        'country': response['sys']['country'],
+        'temperature': round(response['main']['temp'] - 273.15),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon']
+    }    
+
+    return render(request, 'city.html', context)
