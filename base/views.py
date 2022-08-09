942042361097fca7a706d86e80502f36c237c659
xMigma <miguelmh04@hotmail.com>
2022-08-09 15:31:07 +0200

    updated to deploy

--- base/views.py
+++ base/views.py
@@ -2,7 +2,6 @@ import requests
 import json
 from urllib import response
 from django.shortcuts import render
-import json
 
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 API_KEY = '267d5a6861d7d54d0c3ed913fec66929'
--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4f9@j*s!t#q5z%s&5g81g8mj%#r+kwih(s*7lw)z2l3(c+4&oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -70,8 +70,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'weatherApp.wsgi.application'
 
+
+WSGI_APPLICATION = 'weatherApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
