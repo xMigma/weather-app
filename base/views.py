d8ede3385b5a165ee2b58b3423d2f43918a91f50
xMigma <miguelmh04@hotmail.com>
2022-08-11 15:13:04 +0200

    Handling errors and refactoring

--- base/views.py
+++ base/views.py
@@ -1,21 +1,21 @@
+from distutils.log import error
+from pydoc import resolve
 import requests
 import json
-from urllib import response
 from django.shortcuts import render
 
-BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
 API_KEY = '267d5a6861d7d54d0c3ed913fec66929'
 
-def index(request):
+def get_city_by_ip():
     ip = requests.get('https://api.ipify.org').text
     res = requests.get('http://ip-api.com/json/' + ip)
+    return res.json()['city']
 
-    if request.method == 'POST':
-        city = request.POST.get('city')
-    else:
-        city = res.json()['city']
+default_city = get_city_by_ip()    
 
-    url = BASE_URL + 'q=' + city + '&appid=' + API_KEY
+def get_weather(request, city, error):
+    url = BASE_URL + city + '&appid=' + API_KEY
     response = requests.get(url).json()
 
     context = {
@@ -28,6 +28,22 @@ def index(request):
         'humidity': response['main']['humidity'],
         'pressure': response['main']['pressure'],
         'wind': round(response['wind']['speed'], 1),
-        
-    }    
-    return render(request, 'city.html', context)
+        'error': error
+    }
+    return context
+
+
+def index(request):
+    global default_city
+    if request.method == 'POST':
+        city = request.POST.get('city')
+    else:
+        city = default_city
+
+    try:
+        url = BASE_URL + city + '&appid=' + API_KEY
+        response = requests.get(url).json()
+        default_city = response['name']
+        return render(request, 'index.html', get_weather(request, city, None))
+    except KeyError:
+        return render(request, 'index.html', get_weather(request, default_city, 'Insert a valid city name'))
--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4f9@j*s!t#q5z%s&5g81g8mj%#r+kwih(s*7lw)z2l3(c+4&oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://migma-weather.up.railway.app']
