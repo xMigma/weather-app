5701d275019f344018da75f8851344f8bb683604
xMigma <miguelmh04@hotmail.com>
2022-08-11 20:47:26 +0200

    Changed debug to false

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -23,7 +23,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://migma-weather.up.railway.app']
