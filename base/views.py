8438d6cd651dd65671783850b2e35f602d064ee7
xMigma <miguelmh04@hotmail.com>
2022-08-12 19:48:23 +0200

    Refactoring

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from distutils.log import error
+from operator import ge
 from pydoc import resolve
 import requests
 import json
@@ -13,38 +14,29 @@ def get_city_by_ip():
     res = requests.get('http://ip-api.com/json/' + ip)
     return res.json()['city']
 
-default_city = get_city_by_ip()    
-
-def get_weather(request, city, error):
-    url = BASE_URL + city + '&appid=' + API_KEY
-    response = requests.get(url).json()
-
-    context = {
-        'city': response['name'],
-        'country': response['sys']['country'],
-        'temperature': round(response['main']['temp'] - 273.15),
-        'min_temp': round(response['main']['temp_min'] - 273.15),
-        'max_temp': round(response['main']['temp_max'] - 273.15),
-        'description': response['weather'][0]['description'],
-        'humidity': response['main']['humidity'],
-        'pressure': response['main']['pressure'],
-        'wind': round(response['wind']['speed'], 1),
-        'error': error
-    }
-    return context
-
-
 def index(request):
-    global default_city
+    global context
     if request.method == 'POST':
         city = request.POST.get('city')
     else:
-        city = default_city
+        city = get_city_by_ip()
 
     try:
         url = BASE_URL + city + '&appid=' + API_KEY
         response = requests.get(url).json()
-        default_city = response['name']
-        return render(request, 'index.html', get_weather(request, city, None))
+        context = {
+            'city': response['name'],
+            'country': response['sys']['country'],
+            'temperature': round(response['main']['temp'] - 273.15),
+            'min_temp': round(response['main']['temp_min'] - 273.15),
+            'max_temp': round(response['main']['temp_max'] - 273.15),
+            'description': response['weather'][0]['description'],
+            'humidity': response['main']['humidity'],
+            'pressure': response['main']['pressure'],
+            'wind': round(response['wind']['speed'], 1),
+        }
+
     except KeyError:
-        return render(request, 'index.html', get_weather(request, default_city, 'Insert a valid city name'))
+        context['error'] = 'Insert a valid city name'
+
+    return render(request, 'index.html', context)
